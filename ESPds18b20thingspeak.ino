430cf49208a0be24e166487cac5c74f2d136d762
thirschbuechler <thirschbuechler@users.noreply.github.com>
2019-04-07 16:06:28 +0200

    more error handling
    of NAN errors and all-zeros

--- ESPds18b20thingspeak.ino
+++ ESPds18b20thingspeak.ino
@@ -2,10 +2,26 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <WiFiManager.h>//no hard-coded wifi credentials
+#include <math.h>  // for "isnan"
 #include "sensorconfig.h"
 //node-config: provides *adresses, *adresses2, key, server
 //        ONE_WIRE_BUS, maxdisc, myPeriodic, thermos
 
+#define sw_version "1.1" 
+
+//// Changelog ////
+// 1.01: renamed sendTeperature to sendTemperature + its 1x call
+// 1.1: added isnan in buildstr
+//       turned down maxloops to 500 (from 1000) after making it a var
+//       added 0.0==total to buildstr reset condition (is very unlikely)
+//       improved reset by using proper reset instruction instead of "0"-address
+
+// ToDo: try-catch? https://discourse.processing.org/t/try-catch-with-serial/1148/6
+// ToDo: check whether resetfunc fails sometimes (worked in lab conditions), e.g. write values -10 to channels
+
+int maxloops = 500;
+
+//EspClass ESP;//gets created somewhere else and would be redeclaration (one of the wifi libs?)
 
 OneWire oneWire(ONE_WIRE_BUS);
 
@@ -34,22 +50,41 @@ void setup() {
   connectWifi();
 }
 
-void(* resetFunc) (void) = 0;//declare reset function at address 0
+//void(* resetFunc) (void) = 0;//declare reset function at address 0
+void resetFunc(void){
+  ESP.restart(); //clean restart with clearing registers instead of lazy reset with dirty registers (=address0)
+  //one issue: first reboot has to be hardware, based, or it hangs with "ets Jan  8 2013,rst cause:2, boot mode:(1,7)"
+  // see https://github.com/esp8266/Arduino/issues/1722
+}
 
-// function to print a device address
-void printAddress(DeviceAddress deviceAddress){
-  
+// function to get a device address as String
+String strAddress(DeviceAddress deviceAddress){
+  String ret="";
   for (uint8_t i = 0; i < 8; i++){
-    if (deviceAddress[i] < 16) Serial.print("0"); // zero pad the address if necessary
-    Serial.print(deviceAddress[i], HEX);
+    if (deviceAddress[i] < 16) ret+="0"; // zero pad the address if necessary
+    ret+=String(deviceAddress[i], HEX);
   }
-  Serial.print("\n");
+  ret+=("\n");
+  return(ret);
+}
+
+//get name if available, this doesn't work for some reason
+String nameforaddress(DeviceAddress deviceAddress){
+  for (uint8_t i = 1; i < (thermos + 1); i++) {
+    if ((unsigned char *)addresses[i-1]==deviceAddress){
+        return(String(names[i-1])+" "+strAddress(deviceAddress));
+    }
+    else{
+        return("unknown: "+strAddress(deviceAddress));
+    }
+  }
+  
 }
 
 // get temperature of sensor $numb as specified in adresses in sensorconfig.h
 float gettempx(int numb) {
   float temp = -127;
-  temp = sensors.getTempC((unsigned char *) adresses[numb]);
+  temp = sensors.getTempC((unsigned char *) addresses[numb]);
   Serial.print(String(temp) + "°C ");
   
   if (temp > 125){
@@ -66,16 +101,54 @@ float gettempx(int numb) {
   }
   else{
     //String tempC = dtostrf(temp, 4, 1, buffer);//handled in sendTemp()
-    Serial.println("loop No." + String(loops) + " Temperature");
-    //Serial.println(temp);
+    Serial.println("Temperature: "+String(temp));
     return (temp);
 
   }//end if/else
 }//end gettempx
 
+String buildstr(){
+  
+    Serial.println("sensor request");
+    sensors.requestTemperatures(); //query all devices
+    Serial.println("sensor request over");
+    
+    String postStr = "";
+  
+    float temp = -127;
+    float total = 0.0;
+    
+    for (uint8_t i = 1; i < (thermos + 1); i++) {
+
+      temp = gettempx(i - 1);
+      
+      if (isnan(temp)){ // reset on NAN
+        resetFunc();
+      }
+      
+      if (temp != -127){ // ignore on not-initialized
+        postStr += "&field";
+        postStr += String(i);
+        postStr += "=";
+        postStr += String(temp);
+        total += temp;
+        
+      }//end temp-good
+   
+
+    }//end for
+
+
+    if ((0.0==total)&&(loops>5)){ //it's so unlikely that a sensor error is more likely --> reset
+      resetFunc();
+    }
+    else{
+      return(postStr);
+    }
+}
 
 //connect to thingspeak, query sensors, upload data
-void sendTeperatureTS(){
+void sendTemperatureTS(){
   WiFiClient client; //client object to connect to server
 
   if (client.connect(server, 80)) {
@@ -85,23 +158,8 @@ void sendTeperatureTS(){
     String apiKey = (char *)key;
     //Serial.println(apiKey);
 
-    Serial.println("sensor request");
-    sensors.requestTemperatures(); //query all devices
-    Serial.println("sensor request over");
 
-    int i = 1;
-    float temp = -127;
-    for (i = 1; i < (thermos + 1); i++) {
-
-      temp = gettempx(i - 1);
-      if (temp != -127){
-        postStr += "&field";
-        postStr += String(i);
-        postStr += "=";
-        postStr += String(temp);
-      }
-
-    }
+    postStr = buildstr();
 
     postStr += "\r\n\r\n";
 
@@ -126,6 +184,9 @@ void sendTeperatureTS(){
 ///// main loop
 void loop() {
   float temp;
+  //oh arduino, why don't you support "%s" ..
+  Serial.println("sw version "+String(sw_version)+" with sensors "+String(sensorconfigname)+", loop Nr."+String(loops));
+
   sensors.begin();//not needed until multiples??//hotswap works
   //#ifdef __PARASITIC//this does not work, oh well, whatever
   //OneWire.write(0x44, 1);        // start conversion, with parasite power on at the end
@@ -145,24 +206,25 @@ void loop() {
       //Serial.println("no Device" +String(i));
     }
     else {
-      Serial.println("device " + String(i + 1) + " found");
-      Serial.println(sensors.getTempCByIndex(i));
-      printAddress(currentaddress);
+      //Serial.println("device " + String(i + 1) + " found");//don't show this random order
+      Serial.println(strAddress(currentaddress)+" "+String(sensors.getTempCByIndex(i)));
+      //Serial.println(nameforaddress(currentaddress));
     }
   }
 
   Serial.println("\naddress discovery over");
 
   loops++;
-  if (loops > 1000) {
+  if (loops > maxloops) {
     resetFunc();
   }
 
   
   if (uploadenabled){
-    sendTeperatureTS();  
+    sendTemperatureTS();  
   }
   else{
+    Serial.println(buildstr());
     Serial.println("Upload disabled for setup readout");
   }
   
